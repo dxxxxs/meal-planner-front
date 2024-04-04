14e81033be0982f5e18929f11daf2b3a43c4384d
oscar <oscar.gonper.3@educa.jcyl.es>
2024-04-04 13:44:49 +0200

    App component, config and routes fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RecipeServiceService } from './_services/recipe.service.service';
+import { RecipeAPIResponse } from './_interfaces/recipe.interface';
+import { RecipeCardComponent } from './components/recipe.card/recipe.card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,RecipeCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tfg-front';
+
+  number = Number;
+  data: RecipeAPIResponse[] = [];
+
+  constructor(private recipeService: RecipeServiceService) {
+    recipeService.getRecipes().subscribe((res) => {
+      this.data?.push(res);
+    });
+  }
+
+  loadNextRecipes() {
+    if (this.data !== undefined) {
+      this.recipeService.getNextRecipes(this.data[this.data.length - 1]._links.next.href).subscribe((res) => {
+        this.data?.push(res);
+      })
+    }
+  }
+
+
 }
