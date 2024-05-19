22e1704c8ef01bb1f40f87dd2a9219cbb52bf9fb
oscar <oscar.gonper.3@educa.jcyl.es>
2024-05-19 02:37:38 +0200

    Modal estilizado

--- src/app/components/modal-recipe/modal-recipe.component.ts
+++ src/app/components/modal-recipe/modal-recipe.component.ts
@@ -1,12 +1,15 @@
+import { DecimalPipe, TitleCasePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Digest, Hit } from '../../_interfaces/recipe.interface';
 
 @Component({
   selector: 'app-modal-recipe',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe, TitleCasePipe],
   templateUrl: './modal-recipe.component.html',
   styleUrl: './modal-recipe.component.scss'
 })
 export class ModalRecipeComponent {
-  @Input() recipeData : any = "";
+  @Input() recipeData?: Hit;
+
 }
