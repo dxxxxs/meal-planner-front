d5efd633b2beaa7955f61947cbacd9ceabce4bb6
oscar <oscar.gonper.3@educa.jcyl.es>
2024-05-26 19:23:02 +0200

    Añadido rutas no contempladas asignadas al componente NotFoundPage creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { PlannerComponent } from './views/planner/planner.component';
 import { RegisterComponent } from './views/register/register.component';
 import { LoginComponent } from './views/login/login.component';
 import { ProfileComponent } from './views/profile/profile.component';
+import { NotFoundPageComponent } from './views/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
     {
@@ -26,5 +27,8 @@ export const routes: Routes = [
         path:'profile',
         component:ProfileComponent
     },
-
+    {
+        path: '**',
+        component: NotFoundPageComponent
+    }
 ];
